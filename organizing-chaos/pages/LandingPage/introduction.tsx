c3181e7ae8f6da8079b55aaf93f36b70b5942108
David Conley <dconley1989@gmail.com>
2023-01-01 10:46:50 -0700

    added paragraph elements, content, and button to introduction page and more css

--- organizing-chaos/pages/LandingPage/introduction.tsx
+++ organizing-chaos/pages/LandingPage/introduction.tsx
@@ -4,9 +4,9 @@ import styles from "../../styles/Home.module.css";
 
 const Introduction = () => {
   return (
-    <div className={styles.upperDiv}>
-      <section>
-        <h1 className={styles.slogan}>Organize Chaos</h1>
+    <div className={styles.main}>
+      <div className={styles.upperDiv}>
+        <h1 className={styles.title}>Organize Chaos</h1>
         <Image
           src={backgroundImg}
           alt="Woman standing in her home"
@@ -17,8 +17,17 @@ const Introduction = () => {
         <h2 className={styles.slogan}>
           Home organization at your fingertips, literally.
         </h2>
-      </section>
-      <section></section>
+      </div>
+      <div className={styles.lowerDiv}>
+        <p className={styles.lowerDivP}>
+          Are you frustrated with to do apps not being customized to help manage
+          every day life? It should be easier, right?
+        </p>
+        {/* <p className={styles.lowerDivP}></p> */}
+        <button className={styles.button}>
+          Click here to change that feeling!
+        </button>
+      </div>
     </div>
   );
 };
