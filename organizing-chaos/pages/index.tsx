9b99f5f6f804a87d33c811d7bf048fc792d6c9ae
David Conley <dconley1989@gmail.com>
2023-01-08 11:07:41 -0700

    added react icons to the landing page and removed the landing page folder to create the pages I need

--- organizing-chaos/pages/index.tsx
+++ organizing-chaos/pages/index.tsx
@@ -1,6 +1,10 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
-import Introduction from "./LandingPage/introduction";
+// import Introduction from "./LandingPage/introduction";
+import Image from "next/image";
+import backgroundImg from "../public/images/bench-accounting-8D2k7a3wMKQ-unsplash.jpg";
+import styles from "../styles/Home.module.css";
+import { ImArrowRight2 } from "react-icons/im";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +21,27 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Introduction />
+        <div className={styles.main}>
+          <div className={styles.upperDiv}>
+            <h1 className={styles.title}>Organize Chaos</h1>
+            <Image
+              src={backgroundImg}
+              alt="Woman standing in her home"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+            />
+          </div>
+          <div className={styles.lowerDiv}>
+            <p className={styles.lowerDivP}>
+              Are you frustrated with the chaos of every day life?
+            </p>
+            <button className={styles.button}>
+              Yes? Start Here
+              <ImArrowRight2 size={16} />
+            </button>
+          </div>
+        </div>
       </main>
     </>
   );
