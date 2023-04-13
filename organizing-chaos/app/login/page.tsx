fece45d585f46464c4a448078d548b49b91d82ec
David Conley <dconley1989@gmail.com>
2023-04-13 16:28:59 -0600

    added a couple new components and started to add state to the login component

--- organizing-chaos/app/login/page.tsx
+++ organizing-chaos/app/login/page.tsx
@@ -1,39 +1,63 @@
 "use client";
-import Layout from "../../components/Layout";
+import { useState } from "react";
 import Header from "../../components/Header";
-import styles from "../styles/Login.module.css";
+import styles from "../../styles/Login.module.css";
 import { ImArrowRight2 } from "react-icons/im";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
+interface Login {
+  email: string;
+  password: string;
+}
+
+const initialDisplay = false;
+
+const initialLogin = {
+  email: "",
+  password: "",
+};
 
 function login() {
+  const [displayPassword, setDisplayPassword] =
+    useState<boolean>(initialDisplay);
+  const [loginCredentials, setLoginCredentials] = useState<Login>(initialLogin);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginCredentials({
+      ...loginCredentials,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
-    <Layout>
-      <div className={styles.container}>
-        <Header className={styles.title}>Log In</Header>
-        <form className={styles.form}>
-          <label className={styles.label} htmlFor="email">
-            Email
-          </label>
-          <input
-            className={styles.input}
-            name="email"
-            placeholder="Email"
-            autoComplete="username"
-          />
-          <label className={styles.label} htmlFor="password">
-            Password
-          </label>
-          <input
-            className={styles.input}
-            name="password"
-            placeholder="Password"
-            autoComplete="current-password"
-          />
-          <button className={styles.button}>
-            Continue <ImArrowRight2 />
-          </button>
-        </form>
-      </div>
-    </Layout>
+    <div className={styles.container}>
+      <Header className={styles.title}>Log In</Header>
+      <form className={styles.form}>
+        <label className={styles.label} htmlFor="email">
+          Email
+        </label>
+        <Input
+          className={styles.input}
+          name="email"
+          placeholder="Email"
+          autoComplete="username"
+          onChange={handleChange}
+        />
+        <label className={styles.label} htmlFor="password">
+          Password
+        </label>
+        <Input
+          className={styles.input}
+          name="password"
+          placeholder="Password"
+          autoComplete="current-password"
+          onChange={handleChange}
+        />
+        <Button className={styles.button}>
+          Continue <ImArrowRight2 />
+        </Button>
+      </form>
+    </div>
   );
 }
 
--- organizing-chaos/app/register/page.tsx
+++ organizing-chaos/app/register/page.tsx
@@ -1,49 +1,47 @@
 "use client";
-import Layout from "../../components/Layout";
 import Header from "../../components/Header";
 import Button from "../../components/Button";
-import styles from "../styles/Register.module.css";
+import styles from "../../styles/Register.module.css";
 import { ImArrowRight2 } from "react-icons/im";
+import Input from "../../components/Input";
 
 function register() {
   return (
-    <Layout>
-      <div className={styles.container}>
-        <Header className={styles.title}>Create an Account</Header>
-        <form className={styles.form}>
-          <label htmlFor="name" className={styles.label}>
-            Name
-          </label>
-          <input
-            className={styles.input}
-            name="name"
-            required
-            autoComplete="name"
-          ></input>
-          <label htmlFor="email" className={styles.label}>
-            Email
-          </label>
-          <input
-            className={styles.input}
-            name="email"
-            required
-            autoComplete="username"
-          ></input>
-          <label htmlFor="password" className={styles.label}>
-            Password
-          </label>
-          <input
-            className={styles.input}
-            required
-            autoComplete="new-password"
-            name="password"
-          ></input>
-          <Button className={styles.button}>
-            Continue <ImArrowRight2 />
-          </Button>
-        </form>
-      </div>
-    </Layout>
+    <div className={styles.container}>
+      <Header className={styles.title}>Create an Account</Header>
+      <form className={styles.form}>
+        <label htmlFor="name" className={styles.label}>
+          Name
+        </label>
+        <Input
+          className={styles.input}
+          name="name"
+          required
+          autoComplete="name"
+        />
+        <label htmlFor="email" className={styles.label}>
+          Email
+        </label>
+        <Input
+          className={styles.input}
+          name="email"
+          required
+          autoComplete="username"
+        />
+        <label htmlFor="password" className={styles.label}>
+          Password
+        </label>
+        <Input
+          className={styles.input}
+          required
+          autoComplete="new-password"
+          name="password"
+        />
+        <Button className={styles.button}>
+          Continue <ImArrowRight2 />
+        </Button>
+      </form>
+    </div>
   );
 }
 
